d4224634508b9f37ed946f0992a26daf718db976
Augu <joseyaugustine18@gmail.com>
2024-10-19 09:32:26 +0530

    update(shadcn): add shadcn

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,36 +1,83 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        primaryColor: "rgb(99, 102, 241)",
-        secondaryColor: "rgb(161, 163, 247)",
-        primaryText: "rgb(255,255,255)",
-        secondaryText: "rgb(174, 178, 183)",
-        bgDark1: "rgb(31, 32, 35)",
-        bgDark2: "rgb(38, 39, 43)",
-        bgDark3: "rgb(48, 49, 54)",
-        bgDark3Hover: "rgb(55, 56, 62)",
-        bgDarkTransparent: "rgb(31, 32, 35, 0.7)",
-        bgDarkTransparentDarker: "rgb(0,0,0,0.5)",
-        bgDarkTransparentLighter: "rgb(48, 49, 54, 0.7)",
-        mainBorder: "rgb(255,255,255,0.15)",
-        mainBorderDarker: "rgb(255,255,255,0.07)",
-        quoteIconColor: "rgb(178, 184, 205)"
-      },
-      fontFamily: {
-        Inter: "Inter",
-      },
-      screens: {
-        xs: "530px",
-        sm: "640px",
-        md: "768px",
-        lg: "1024px",
-        xl: "1280px",      
-        "2xl": "1536px",
-      },
-    },
+  	extend: {
+  		colors: {
+  			primaryColor: 'rgb(99, 102, 241)',
+  			secondaryColor: 'rgb(161, 163, 247)',
+  			primaryText: 'rgb(255,255,255)',
+  			secondaryText: 'rgb(174, 178, 183)',
+  			bgDark1: 'rgb(31, 32, 35)',
+  			bgDark2: 'rgb(38, 39, 43)',
+  			bgDark3: 'rgb(48, 49, 54)',
+  			bgDark3Hover: 'rgb(55, 56, 62)',
+  			bgDarkTransparent: 'rgb(31, 32, 35, 0.7)',
+  			bgDarkTransparentDarker: 'rgb(0,0,0,0.5)',
+  			bgDarkTransparentLighter: 'rgb(48, 49, 54, 0.7)',
+  			mainBorder: 'rgb(255,255,255,0.15)',
+  			mainBorderDarker: 'rgb(255,255,255,0.07)',
+  			quoteIconColor: 'rgb(178, 184, 205)',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			Inter: 'Inter'
+  		},
+  		screens: {
+  			xs: '530px',
+  			sm: '640px',
+  			md: '768px',
+  			lg: '1024px',
+  			xl: '1280px',
+  			'2xl': '1536px'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
+    plugins: [require("tailwindcss-animate")]
 };
 
