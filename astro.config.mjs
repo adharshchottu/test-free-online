e9a7daaa1a100b60a8e83a9449356d13dea53c05
Augu <joseyaugustine18@gmail.com>
2024-12-19 18:42:11 +0530

    feat(sitemap): implement sitemap generation as an API route and remove old sitemap component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from "@astrojs/tailwind";
-import sitemap from "@astrojs/sitemap";
 
 import svelte from '@astrojs/svelte';
 
@@ -11,12 +10,6 @@ import vercel from '@astrojs/vercel';
 
 export default defineConfig({
   site: 'https://www.test-free.online',
-  integrations: [react(), tailwind(), sitemap(), svelte(), vue()],
-  adapter: vercel(),
-  routes: [
-    {
-      path: '/sitemap.xml',
-      entry: './src/components/sitemap.astro',
-    },
-  ],
+  integrations: [react(), tailwind(), svelte(), vue()],
+  adapter: vercel()
 });
