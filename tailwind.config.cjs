3b7466505a6630296102164a313210d06b780cd9
Augu <joseyaugustine18@gmail.com>
2026-06-12 22:40:24 +0530

    feat: add tweet button functionality and corresponding styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,7 @@ module.exports = {
   		colors: {
   			primaryColor: 'rgb(99, 102, 241)',
   			secondaryColor: 'rgb(161, 163, 247)',
+			tweetBlue: 'rgb(29, 155, 240)',
   			primaryText: 'rgb(255,255,255)',
   			secondaryText: 'rgb(174, 178, 183)',
   			bgDark1: 'rgb(31, 32, 35)',
